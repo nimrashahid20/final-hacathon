58e3b036dfda6d8344022eda6fbf105176d0c3a9
mariyam saleem <mariyamsaleem87@gmail.com>
2025-03-21 15:06:05 +0500

    delete button

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUserPen } from "@fortawesome/free-solid-svg-icons";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
@@ -60,13 +62,45 @@ const Profile = () => {
     );
   }
 
+  let deleteUser = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/auth/user/${id}`, {
+        method: "DELETE"
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        console.log("User deleted:", result);
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
+        navigate("/signup");
+      } else {
+        toast.error(data.message || "Failed to delete user");
+      }
+    }
+    catch (error) {
+      console.error("Error:", error);
+      toast.error("An error occurred while delete user");
+    }
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white rounded-lg shadow-lg overflow-hidden">
         {/* Card Header */}
-        <div className="bg-gray-800 text-white p-6">
-          <h2 className="text-2xl font-semibold">Profile Details</h2>
-          <p className="text-gray-300">Welcome back, {user.name}!</p>
+        <div className="bg-gray-800 text-white p-6 flex">
+          <p className="text-gray-300 w-64 flex-1 ">Welcome back, {user.name}!</p>
+          <div>
+            <FontAwesomeIcon
+              icon={faUserPen}
+              className="h-6 w-6 sm:h-8 sm:w-8 hover:text-[#4bf6d4] transition duration-300"
+            />
+            <FontAwesomeIcon
+              icon={faTrash}
+              className="h-6 w-6 sm:h-8 sm:w-8 hover:text-[#4bf6d4] transition duration-300"
+              onClick={async () => { await deleteUser(user._id) }}
+            />
+          </div>
         </div>
 
         {/* Card Body */}
