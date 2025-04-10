a2af8d924fff7ef06cfdb6b7fb2ea8fb9530d792
Umra-Badar20 <umrabadar555@gmail.com>
2025-04-10 17:19:18 +0500

    Vercel url added

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173'],
+		origin: ['http://localhost:5174',
+			 'http://localhost:5173',
+			 'https://ecommerce-app-three-phi.vercel.app/'
+			],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
